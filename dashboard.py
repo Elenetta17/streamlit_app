793f80254e7252ef0b5d2d9a772863c9b47eccc7
Elena Nardi <elenanardi@hotmail.com>
2023-04-03 17:32:26 +0200

    Envoi id client API

--- dashboard.py
+++ dashboard.py
@@ -8,6 +8,11 @@ st.write("""
 """"""
 
 client_list = requests.get('http://127.0.0.1:3000/get_clients_id').json()['data']
-print(client_list)
 sorted_client_list = sorted(client_list)
 selected_client = st.sidebar.selectbox('Clients_id', sorted_client_list)
+
+
+url = 'http://127.0.0.1:3000/predict'
+client_id = str(selected_client)
+x = requests.post(url, data = client_id)
+st.write(x.text)
