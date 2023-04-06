23df81c7dc73ffce1e2a07df10d139c998d58faa
Elena Nardi <elenanardi@hotmail.com>
2023-04-06 10:39:32 +0200

    ajout boxplots

--- .ipynb_checkpoints/dashboard-checkpoint.py
+++ .ipynb_checkpoints/dashboard-checkpoint.py
@@ -1,5 +1,11 @@
+import pandas as pd
 import streamlit as st
 import requests
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+X_test_reduced = pd.read_csv('../X_test_reduced.csv', index_col=0) 
 
 st.write("""
 # Modèle de scoring""")
@@ -18,3 +24,22 @@ prediction = requests.post(url, data = client_id)
 print(prediction)
 st.write("""
 ## Probabilité de remboursement: """ + prediction.text + " %")
+
+# Store the list of columns
+columns_to_plot = ['EXT_SOURCE_2', 'EXT_SOURCE_3', 'INSTAL_AMT_PAYMENT_SUM', 'DAYS_EMPLOYED',
+                    'NAME_EDUCATION_TYPE_Higher education', 'INSTAL_DPD_MEAN','POS_COUNT', 'DAYS_BIRTH','AMT_CREDIT','AMT_ANNUITY'
+                                 ]
+st.write("""
+## Positionnement du client par rapport à l'ensamble de clients """)
+# boxplots
+fig, ax = plt.subplots((len(columns_to_plot)+1)//2, 2, figsize=(15, 15))
+count = 1
+for col in columns_to_plot:
+    plt.subplot((len(columns_to_plot)+1)//2, 2, count)
+    plt.title(col)
+    sns.boxplot(data=X_test_reduced, x=col, showfliers=False)
+    plt.scatter(X_test_reduced.loc[selected_client, col],0, linewidths=5, c='red')
+    plt.text(X_test_reduced.loc[selected_client, col],-0.1, "CLIENT")
+    count += 1
+plt.tight_layout()
+st.pyplot(fig)
