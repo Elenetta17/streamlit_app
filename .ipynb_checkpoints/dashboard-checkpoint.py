fa2ede77e04ae7bd4ca83ccd726334a96b776956
Elena Nardi <elenanardi@hotmail.com>
2023-04-18 14:46:27 +0200

    ajout description variables

--- .ipynb_checkpoints/dashboard-checkpoint.py
+++ .ipynb_checkpoints/dashboard-checkpoint.py
@@ -7,6 +7,7 @@ import requests
 import shap
 import streamlit as st
 
+variables=pd.read_csv('description_variables.csv', sep = '\t', index_col=0)
 
 @st.cache_data # caching
 def load_dataframe(path):
@@ -31,10 +32,15 @@ def load_client_list(data):
     return sorted(data.index)
 
 # chargement dataframe
-data = load_dataframe('test_kaggle_reduced.csv') 
+data = load_dataframe('kaggle_reduced.csv') 
 
 # chargement explainer et shap values
 explainer, shap_values = load_shap_values('explainer.pkl', data)
+
+shap_object = shap.Explanation(base_values = explainer.expected_value[0],
+values = shap_values[0],
+feature_names = data.columns,
+data = data)
 
 # categorical variables
 categorical_columns=[
@@ -60,8 +66,18 @@ client_list = load_client_list(data)
 selected_client = st.sidebar.selectbox('Clients_id', client_list)
 
 # add features to sidebar
-features_to_show = st.sidebar.multiselect('Variables', sorted(data.columns), default = ['EXT_SOURCE_2', 'EXT_SOURCE_3', 'INSTAL_AMT_PAYMENT_SUM', 'DAYS_EMPLOYED',
-                    'NAME_EDUCATION_TYPE_Higher education', 'INSTAL_DPD_MEAN'],
+features_to_show = st.sidebar.multiselect('Variables', sorted(data.columns),
+                                          default = [
+                                              'AMT_CREDIT',
+ 'BURO_DAYS_CREDIT_ENDDATE_MAX',
+ 'DAYS_BIRTH',
+ 'DAYS_EMPLOYED',
+ 'EXT_SOURCE_2',
+ 'EXT_SOURCE_3',
+ 'NAME_EDUCATION_TYPE_Higher education',
+ 'PAYMENT_RATE',
+ 'PREV_CNT_PAYMENT_MEAN',
+ 'PREV_NAME_YIELD_GROUP_high_MEAN'],
                                          max_selections=10)
 #=======================================================================#
 ### PREDICTION ####
@@ -69,18 +85,25 @@ st.write("""
 ## Prédiction """)
 
 # get prediction
-#url = 'http://127.0.0.1:3000/predict'
-url= 'https://elena-openclassrooms-predict.herokuapp.com/predict'
+url = 'http://127.0.0.1:3000/predict'
+# url= 'https://elena-openclassrooms-predict.herokuapp.com/predict'
 client_id = str(selected_client)
 prediction = requests.post(url, data=client_id)
 
 col1, col2 = st.columns(2)
 
 # Showing client state and probability 
-etat_client = 'Client peu risqué' if int(prediction.text) > 50 else 'Client à risque de defaut'
+if int(prediction.text) > 50:
+    client_state = 'Client peu risqué'
+    approval = 'Accorder le crédit'
+else:
+    client_state = 'Client à risque de défaut'
+    approval = 'Refuser le crédit'
+
 col1.subheader("""Client """ + client_id)
 col1.write("""Probabilité de remboursement: """ + prediction.text +"%")
-col1.write("""Etat client: **""" + etat_client + """**""")
+col1.write("""Etat client: **""" + client_state + """**""")
+col1.write("""**""" + approval + """**""")
 
 # Gauge graph
 fig = go.Figure(go.Indicator(
@@ -91,7 +114,7 @@ fig = go.Figure(go.Indicator(
     title = {'text': "Probabilité de remboursement", 'font':{'color':'dimgrey' }},
     gauge = {
         'axis': {'range': [None, 100], 'tickwidth': 1, 'tickcolor': "dimgrey", 'tickfont':{'color':'dimgrey' }},
-        'bar': {'color': "dimgrey"},
+        'bar': {'color': "lightgrey"},
         'steps': [
             {'range': [0, 50], 'color': 'red'},
             {'range': [50, 100], 'color': 'green'}],
@@ -113,24 +136,24 @@ st.write("""
 # list the values of selected features
 for feature in features_to_show:
     if feature not in categorical_columns:
-        st.write("""**"""+feature+""":** """ + str(data.loc[selected_client, feature]))
+        st.markdown("""**"""+feature+""": """ + str(round(data.loc[selected_client, feature], 2))+ '** - <span style=font-size:14px>' + variables.loc[feature, 'Description'], unsafe_allow_html=True)
     else:  # if feature is categorical, display Yes or No
         if data.loc[selected_client, feature] > 0.5:
-            st.write("""**"""+feature+""":** Oui""")
+            st.markdown("""**"""+feature+""": Oui**"""+ ' - <span style=font-size:14px>' + variables.loc[feature, 'Description'], unsafe_allow_html=True)
         else:
-            st.write("""**"""+feature+""":** Non""")
+            st.markdown("""**"""+feature+""":** Non"""  '<span style=font-size:14px>(' + variables.loc[feature, 'Description'] +')', unsafe_allow_html=True) 
+
             
 #=======================================================================#
-
-shap_object = shap.Explanation(base_values = explainer.expected_value[0],
-values = shap_values[0],
-feature_names = data.columns,
-data = data)
+# MODEL EXPLICATION
 
 st.write("""
-## Caractéristiques influençant le score""")
-st_shap(shap.summary_plot(shap_values[0], data,  max_display=10, show=False))
-st_shap(shap.plots.waterfall(shap_object[data.index.get_loc(selected_client)], max_display=10)
+## Explication du modèle""")
+
+st.write("""
+###  Caractéristiques influençant le score du client""")
+st_shap(shap.plots.waterfall(shap_object[data.index.get_loc(selected_client)], max_display=10),
+         width=800, height=300
 )
 
 # Store the list of columns
@@ -144,9 +167,9 @@ for col in features_to_show:
     plt.subplot((len(features_to_show)+1)//2, 2, count)
     plt.title(col)
     plt.hist(data[col])
-    plt.axvline(data.loc[selected_client, col], color='red', linestyle='dashed', linewidth=1)
+    plt.axvline(data.loc[selected_client, col], color='red', linestyle='dashed', linewidth=2)
     min_ylim, max_ylim = plt.ylim()
-    plt.text(data.loc[selected_client, col], max_ylim*0.9, "CLIENT")
+    #plt.text(data.loc[selected_client, col], max_ylim*0.9, "CLIENT")
     count += 1
 if len(features_to_show)%2 !=0:
     fig.delaxes(ax[(len(features_to_show)+1)//2-1][1])
